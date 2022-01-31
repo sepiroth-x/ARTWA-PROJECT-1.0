5acfd8b07a82298f14c75ee2f65191c95f9cf23a
sepiroth-x <45693885+sepiroth-x@users.noreply.github.com>
2022-02-01 01:18:35 +0800

    Update search.php

--- search.php
+++ search.php
@@ -4,9 +4,12 @@ session_start();
 include("connection.php");
 include("functions.php");
 
-
+/** UNDER CONSTRUCTION
 $keyword_entered = $_POST['search-box'];
 $query = "select * from users, user_account_info where ";
 
 
 $mysqli_query($con , $query);
+
+
+**/
